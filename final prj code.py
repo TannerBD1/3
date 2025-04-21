98585f8ca3c488ee7da3b2da22b43108e6fa9272
TannerBD1 <beamiestspider@gmail.com>
2025-04-21 15:08:48 -0600

    ain

--- final prj code.py
+++ final prj code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from markupsafe import Markup
 import random
 import mysql.connector
 import configparser
 from flask_httpauth import HTTPBasicAuth
 import hashlib
+import os
 
 app = Flask(__name__)
 
@@ -82,6 +83,27 @@ def login():
         <h1>Login failed. Invalid credentials.</h1>
                           """)
         return render_template('template.html', titleText=titleText, bodyText=bodyText)
+
+@app.route('/upload', methods=['POST'])
+def check_image_hash():
+    if 'image' not in request.files:
+      return jsonify({'error': 'No image uploaded'}), 400
+
+    file = request.files['image']
+    file_bytes = file.read()
+    return render_template('checkimagehash.html')
+@app.route('/checkimagehash', methods=['GET'])
+def get_md5_hash():
+    file_path = os.path.join(app.static_folder, 'images', 'Dropped Image.png')
+    md5_hash = hashlib.md5()
+    with open(file_path, "rb") as f:
+      for chunk in iter(lambda: f.read(4096), b""):
+        md5_hash.update(chunk)
+    md5_hash = md5_hash.hexdigest()
+    STORED_HASH = md5_hash
+    is_match = (md5_hash == STORED_HASH)
+    print(md5_hash)
+    return render_template('checkimagehash.html', STORED_HASH=STORED_HASH, md5_hash=md5_hash, is_match=is_match)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
